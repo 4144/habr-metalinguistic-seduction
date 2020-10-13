7e10ccd8f5bc9d21e6d27d598f36bde511f5968e
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-10-13 17:43:07 +0600

    Format test_rectangle.c

--- testing-framework/test_rectangle.c
+++ testing-framework/test_rectangle.c
@@ -2,15 +2,12 @@
 #include "test.h"
 
 TESTING(
-    // clang-format off
-TEST(test_area) {
-    Rectangle rectangle = {.height = 15, .width = 8};
-    ASSERT(area(&rectangle) == 15 * 8);
-}
+    TEST(test_area) {
+        Rectangle rectangle = {.height = 15, .width = 8};
+        ASSERT(area(&rectangle) == 15 * 8);
+    }
 
-TEST(test_perimeter) {
-    Rectangle rectangle = {.height = 61, .width = 7};
-    ASSERT(perimeter(&rectangle) == (61 + 7) * 2);
-}
-    // clang-format on
-)
+    TEST(test_perimeter) {
+        Rectangle rectangle = {.height = 61, .width = 7};
+        ASSERT(perimeter(&rectangle) == (61 + 7) * 2);
+    })
