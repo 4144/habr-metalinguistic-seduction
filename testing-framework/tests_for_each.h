f220bfa46cb7f27c5cc86ae28e49fdeb473a6ece
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-10-13 18:06:40 +0600

    Replace CONSUME with STOP in tests_for_each.h

--- testing-framework/tests_for_each.h
+++ testing-framework/tests_for_each.h
@@ -6,25 +6,25 @@
 #define TESTS_FOR_EACH(op, ...) TESTS_FOR_EACH_0(op, __VA_ARGS__, end(), end())
 
 #define TESTS_FOR_EACH_0(op, head, ...)                                                            \
-    IF(TESTS_FOR_EACH_IS_END(head), CONSUME, TESTS_FOR_EACH_0_PROGRESS)(op, head, __VA_ARGS__)
+    IF(TESTS_FOR_EACH_IS_END(head), STOP, TESTS_FOR_EACH_0_PROGRESS)(op, head, __VA_ARGS__)
 #define TESTS_FOR_EACH_1(op, head, ...)                                                            \
-    IF(TESTS_FOR_EACH_IS_END(head), CONSUME, TESTS_FOR_EACH_1_PROGRESS)(op, head, __VA_ARGS__)
+    IF(TESTS_FOR_EACH_IS_END(head), STOP, TESTS_FOR_EACH_1_PROGRESS)(op, head, __VA_ARGS__)
 #define TESTS_FOR_EACH_2(op, head, ...)                                                            \
-    IF(TESTS_FOR_EACH_IS_END(head), CONSUME, TESTS_FOR_EACH_2_PROGRESS)(op, head, __VA_ARGS__)
+    IF(TESTS_FOR_EACH_IS_END(head), STOP, TESTS_FOR_EACH_2_PROGRESS)(op, head, __VA_ARGS__)
 #define TESTS_FOR_EACH_3(op, head, ...)                                                            \
-    IF(TESTS_FOR_EACH_IS_END(head), CONSUME, TESTS_FOR_EACH_3_PROGRESS)(op, head, __VA_ARGS__)
+    IF(TESTS_FOR_EACH_IS_END(head), STOP, TESTS_FOR_EACH_3_PROGRESS)(op, head, __VA_ARGS__)
 #define TESTS_FOR_EACH_4(op, head, ...)                                                            \
-    IF(TESTS_FOR_EACH_IS_END(head), CONSUME, TESTS_FOR_EACH_4_PROGRESS)(op, head, __VA_ARGS__)
+    IF(TESTS_FOR_EACH_IS_END(head), STOP, TESTS_FOR_EACH_4_PROGRESS)(op, head, __VA_ARGS__)
 #define TESTS_FOR_EACH_5(op, head, ...)                                                            \
-    IF(TESTS_FOR_EACH_IS_END(head), CONSUME, TESTS_FOR_EACH_5_PROGRESS)(op, head, __VA_ARGS__)
+    IF(TESTS_FOR_EACH_IS_END(head), STOP, TESTS_FOR_EACH_5_PROGRESS)(op, head, __VA_ARGS__)
 #define TESTS_FOR_EACH_6(op, head, ...)                                                            \
-    IF(TESTS_FOR_EACH_IS_END(head), CONSUME, TESTS_FOR_EACH_6_PROGRESS)(op, head, __VA_ARGS__)
+    IF(TESTS_FOR_EACH_IS_END(head), STOP, TESTS_FOR_EACH_6_PROGRESS)(op, head, __VA_ARGS__)
 #define TESTS_FOR_EACH_7(op, head, ...)                                                            \
-    IF(TESTS_FOR_EACH_IS_END(head), CONSUME, TESTS_FOR_EACH_7_PROGRESS)(op, head, __VA_ARGS__)
+    IF(TESTS_FOR_EACH_IS_END(head), STOP, TESTS_FOR_EACH_7_PROGRESS)(op, head, __VA_ARGS__)
 #define TESTS_FOR_EACH_8(op, head, ...)                                                            \
-    IF(TESTS_FOR_EACH_IS_END(head), CONSUME, TESTS_FOR_EACH_8_PROGRESS)(op, head, __VA_ARGS__)
+    IF(TESTS_FOR_EACH_IS_END(head), STOP, TESTS_FOR_EACH_8_PROGRESS)(op, head, __VA_ARGS__)
 #define TESTS_FOR_EACH_9(op, head, ...)                                                            \
-    IF(TESTS_FOR_EACH_IS_END(head), CONSUME, TESTS_FOR_EACH_9_PROGRESS)(op, head, __VA_ARGS__)
+    IF(TESTS_FOR_EACH_IS_END(head), STOP, TESTS_FOR_EACH_9_PROGRESS)(op, head, __VA_ARGS__)
 
 #define TESTS_FOR_EACH_0_PROGRESS(op, head, ...)                                                   \
     op(TEST_NAME(head), TEST_BODY(head)) TESTS_FOR_EACH_1(op, __VA_ARGS__)
@@ -46,6 +46,8 @@
     op(TEST_NAME(head), TEST_BODY(head)) TESTS_FOR_EACH_9(op, __VA_ARGS__)
 #define TESTS_FOR_EACH_9_PROGRESS(op, head, ...)                                                   \
     op(TEST_NAME(head), TEST_BODY(head)) TESTS_FOR_EACH_10_LIMIT_REACHED(op, __VA_ARGS__)
+
+#define STOP CONSUME
 
 #define TEST_NAME(test)                    MATCH(TEST_NAME_AUX_, test)
 #define TEST_NAME_AUX_test(test_name, ...) test_name
