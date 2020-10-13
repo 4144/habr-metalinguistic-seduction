e6eae21a1d05d64410186cbead93e7009bcc61dc
Temirkhan Myrzamadi <hirrolot@gmail.com>
2020-10-13 17:56:01 +0600

    Increase the limit of tests to 10

--- testing-framework/tests_for_each.h
+++ testing-framework/tests_for_each.h
@@ -13,6 +13,18 @@
     IF(TESTS_FOR_EACH_IS_END(head), CONSUME, TESTS_FOR_EACH_2_PROGRESS)(op, head, __VA_ARGS__)
 #define TESTS_FOR_EACH_3(op, head, ...)                                                            \
     IF(TESTS_FOR_EACH_IS_END(head), CONSUME, TESTS_FOR_EACH_3_PROGRESS)(op, head, __VA_ARGS__)
+#define TESTS_FOR_EACH_4(op, head, ...)                                                            \
+    IF(TESTS_FOR_EACH_IS_END(head), CONSUME, TESTS_FOR_EACH_4_PROGRESS)(op, head, __VA_ARGS__)
+#define TESTS_FOR_EACH_5(op, head, ...)                                                            \
+    IF(TESTS_FOR_EACH_IS_END(head), CONSUME, TESTS_FOR_EACH_5_PROGRESS)(op, head, __VA_ARGS__)
+#define TESTS_FOR_EACH_6(op, head, ...)                                                            \
+    IF(TESTS_FOR_EACH_IS_END(head), CONSUME, TESTS_FOR_EACH_6_PROGRESS)(op, head, __VA_ARGS__)
+#define TESTS_FOR_EACH_7(op, head, ...)                                                            \
+    IF(TESTS_FOR_EACH_IS_END(head), CONSUME, TESTS_FOR_EACH_7_PROGRESS)(op, head, __VA_ARGS__)
+#define TESTS_FOR_EACH_8(op, head, ...)                                                            \
+    IF(TESTS_FOR_EACH_IS_END(head), CONSUME, TESTS_FOR_EACH_8_PROGRESS)(op, head, __VA_ARGS__)
+#define TESTS_FOR_EACH_9(op, head, ...)                                                            \
+    IF(TESTS_FOR_EACH_IS_END(head), CONSUME, TESTS_FOR_EACH_9_PROGRESS)(op, head, __VA_ARGS__)
 
 #define TESTS_FOR_EACH_0_PROGRESS(op, head, ...)                                                   \
     op(TEST_NAME(head), TEST_BODY(head)) TESTS_FOR_EACH_1(op, __VA_ARGS__)
@@ -22,6 +34,18 @@
     op(TEST_NAME(head), TEST_BODY(head)) TESTS_FOR_EACH_3(op, __VA_ARGS__)
 #define TESTS_FOR_EACH_3_PROGRESS(op, head, ...)                                                   \
     op(TEST_NAME(head), TEST_BODY(head)) TESTS_FOR_EACH_4(op, __VA_ARGS__)
+#define TESTS_FOR_EACH_4_PROGRESS(op, head, ...)                                                   \
+    op(TEST_NAME(head), TEST_BODY(head)) TESTS_FOR_EACH_5(op, __VA_ARGS__)
+#define TESTS_FOR_EACH_5_PROGRESS(op, head, ...)                                                   \
+    op(TEST_NAME(head), TEST_BODY(head)) TESTS_FOR_EACH_6(op, __VA_ARGS__)
+#define TESTS_FOR_EACH_6_PROGRESS(op, head, ...)                                                   \
+    op(TEST_NAME(head), TEST_BODY(head)) TESTS_FOR_EACH_7(op, __VA_ARGS__)
+#define TESTS_FOR_EACH_7_PROGRESS(op, head, ...)                                                   \
+    op(TEST_NAME(head), TEST_BODY(head)) TESTS_FOR_EACH_8(op, __VA_ARGS__)
+#define TESTS_FOR_EACH_8_PROGRESS(op, head, ...)                                                   \
+    op(TEST_NAME(head), TEST_BODY(head)) TESTS_FOR_EACH_9(op, __VA_ARGS__)
+#define TESTS_FOR_EACH_9_PROGRESS(op, head, ...)                                                   \
+    op(TEST_NAME(head), TEST_BODY(head)) TESTS_FOR_EACH_10_LIMIT_REACHED(op, __VA_ARGS__)
 
 #define TEST_NAME(test)                    MATCH(TEST_NAME_AUX_, test)
 #define TEST_NAME_AUX_test(test_name, ...) test_name
